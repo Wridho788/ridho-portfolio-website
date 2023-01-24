f61b4a9b98d2573633c4646df7b53ead33408b91
Wridho788 <wridho246@gmail.com>
2023-01-24 14:02:05 +0700

    feature home done

---
+++ src/components/Home/Social.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const Social = () => {
+  return (
+    <div className='home_social'>
+      <a
+        href='https://www.instagram.com/
+'
+        className='home_social-icon'
+        target='_blank' rel="noreferrer">
+        <i className='uil uil-instagram'></i>
+      </a>
+      <a
+        href='https://www.linkedin.com/in/ridho-wahyu-nugroho-4a1544142/'
+        className='home_social-icon'
+        target='_blank' rel="noreferrer">
+        <i class='uil uil-linkedin'></i>
+      </a>
+      <a
+        href='https://github.com/Wridho788'
+        className='home_social-icon'
+        target='_blank' rel="noreferrer">
+        <i className='uil uil-github'></i>
+      </a>
+    </div>
+  );
+};
+
+export default Social;
