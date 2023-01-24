8e3eaada2439bcad52bcc5a07a9449a0dce20d6f
Wridho788 <wridho246@gmail.com>
2023-01-24 16:08:21 +0700

    add info

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import React from 'react'
 
 import "./App.css"
 import Header from "./components/Header/Header"
+import About from './components/About/About';
 
 const App = () => {
   return (
     <>
       <Header />
+      <main className='main'>
+        <About />
+      </main>
     </>
   )
 }
---
+++ src/components/About/Info.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const Info = () => {
+  return (
+    <div className='about_info grid'>
+      <div className='about_box'>
+        <i class='bx bx-award about_icon'></i>
+        <h3 className='about_title'>Experience</h3>
+        <span className='about_subtitle'>3 Years Working</span>
+      </div>
+      <div className='about_box'>
+        <i class='bx bx-briefcase-alt about_icon'></i>
+        <h3 className='about_title'>Complete</h3>
+        <span className='about_subtitle'>3 Project</span>
+      </div>
+      <div className='about_box'>
+        <i class='bx bx-support about_icon'></i>
+        <h3 className='about_title'>Support</h3>
+        <span className='about_subtitle'>Online 24/7</span>
+      </div>
+    </div>
+  );
+};
+
+export default Info;
