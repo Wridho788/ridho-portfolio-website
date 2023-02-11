f024c89d9408597b4eecc296055384f6a2e3e063
Wridho788 <wridho246@gmail.com>
2023-02-11 15:27:09 +0700

    add img

--- src/components/work/Data.jsx
+++ src/components/work/Data.jsx
@@ -1,38 +1,38 @@
-import Work1 from '../../assets/work1.jpg';
-import Work2 from '../../assets/work2.jpg';
-import Work3 from '../../assets/work3.jpg';
-import Work4 from '../../assets/work4.jpg';
-import Work5 from '../../assets/work5.jpg';
+import Work1 from '../../assets/web1.png';
+import Work2 from '../../assets/mobile1.png';
+import Work3 from '../../assets/mobile2.jpeg';
+import Work4 from '../../assets/mobile3.jpeg';
+import Work5 from '../../assets/web2.png';
 
 export const projectsData = [
   {
     id: 1,
     image: Work1,
-    title: 'Web design',
+    title: 'Social Media App',
     category: 'Web',
   },
   {
     id: 2,
     image: Work2,
-    title: 'App movil',
+    title: 'Antar Laundry App',
     category: 'App',
   },
   {
     id: 3,
     image: Work3,
-    title: 'Brand design',
-    category: 'Design',
+    title: 'Stock Opname',
+    category: 'App',
   },
   {
     id: 4,
     image: Work4,
-    title: 'App movil',
+    title: 'Nirvana App',
     category: 'App',
   },
   {
     id: 5,
     image: Work5,
-    title: 'Web design',
+    title: 'Survey App',
     category: 'Web',
   },
 ];
@@ -41,6 +41,4 @@ export const projectsNav = [
     {name: 'All'},
     {name: 'Web'},
     {name: 'App'},
-    {name: 'Design'},
-
 ]
