02ef837fe22985aadc6cf9a38bd552b0ea9f2c09
Wridho788 <wridho246@gmail.com>
2023-02-23 16:26:06 +0700

    update change

--- src/components/work/Data.jsx
+++ src/components/work/Data.jsx
@@ -1,9 +1,10 @@
 import Work1 from '../../assets/web1.png';
 import Work2 from '../../assets/mobile1.png';
-import Work3 from '../../assets/mobile2.jpeg';
+import Work3 from '../../assets/mobile4.png';
 import Work4 from '../../assets/mobile3.jpeg';
 import Work5 from '../../assets/web2.png';
 import Work6 from '../../assets/web3.png';
+import Work7 from '../../assets/web4.png';
 
 export const projectsData = [
   {
@@ -11,47 +12,50 @@ export const projectsData = [
     image: Work1,
     title: 'Social Media App',
     category: 'Web',
-    link: 'https://github.com/Wridho788/merng-client'
+    link: 'https://github.com/Wridho788/merng-client',
   },
   {
     id: 2,
     image: Work2,
     title: 'Antar Laundry App',
     category: 'App',
-    link: 'https://github.com/Wridho788/AntarLaundryApp'
+    link: 'https://github.com/Wridho788/AntarLaundryApp',
   },
   {
     id: 3,
     image: Work3,
-    title: 'Stock Opname',
+    title: 'Kerjaloka',
     category: 'App',
-    link: ''
+    link: 'https://play.google.com/store/apps/details?id=com.ciptakerjaarunika.kerjaloka',
   },
   {
     id: 4,
     image: Work4,
     title: 'Nirvana App',
     category: 'App',
-    link: 'https://github.com/Wridho788/nirvanaApp-v1'
+    link: 'https://play.google.com/store/apps/details?id=id.nirvanastore',
   },
   {
     id: 5,
     image: Work5,
     title: 'Survey App',
     category: 'Web',
-    link: 'https://github.com/Wridho788/Laravel_vue_course'
+    link: 'https://github.com/Wridho788/Laravel_vue_course',
   },
   {
     id: 6,
     image: Work6,
     title: 'Nirvana Store',
     category: 'Web',
-    link: 'https://nirvana.co.id/'
+    link: 'https://nirvana.co.id/',
+  },
+  {
+    id: 7,
+    image: Work7,
+    title: 'React Admin Dashboard',
+    category: 'Web',
+    link: 'https://react-admin-dashboard-pi.vercel.app/',
   },
 ];
 
-export const projectsNav = [
-    {name: 'All'},
-    {name: 'Web'},
-    {name: 'App'},
-]
+export const projectsNav = [{ name: 'All' }, { name: 'Web' }, { name: 'App' }];
